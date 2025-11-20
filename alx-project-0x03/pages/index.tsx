36208fad964a8e4b1d0041d7b777f9b1b1ba60ae
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-20 19:20:23 +0300

    Add welcome

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center pt-16">
-            <h1 className="text-4xl font-bold text-gray-800">Welcome to Splash App</h1>
+        <div>
+            <hi> Welcome to Splash App </hi>
         </div>
     );
 }
