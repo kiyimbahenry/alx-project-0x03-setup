9fc905cf83727a5fc5e1b849fc2c1a222de07238
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-20 19:11:51 +0300

    Add styles directory with Montserrat font and update home page

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center pt-16">
+            <h1 className="text-4xl font-bold text-gray-800">Welcome to Splash App</h1>
+        </div>
+    );
+}
+
+export default Home;
